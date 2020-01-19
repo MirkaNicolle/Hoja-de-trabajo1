de8cc82e8b7af0730adfd88415f4c23254deb2c3
danielavillamar <46539048+danielavillamar@users.noreply.github.com>
2020-01-19 12:46:18 -0600

    Delete class

--- Main.java
+++
@@ -1,7 +0,0 @@
-// Universidad del Valle de Guatemala //
-// Algoritmos y Estructuras de datos  //
-// Sección 70                         //
-// 									  //
-// M. Alejandro Alvarez Vidal - 12429 //
-// Daniela Villamar - 19086			      //
-// Mirka Monzon                       //
