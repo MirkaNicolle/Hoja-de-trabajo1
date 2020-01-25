772f93de1feaa5b73b43085b9e68a7c65867dd7d
alejandroav93 <59927580+alejandroav93@users.noreply.github.com>
2020-01-25 17:50:16 -0600

    Repeticion de Hoja de Trabajo

---
+++ Repeticion Hoja de Trabajo 1/driverRadio.java
@@ -0,0 +1,24 @@
+//**************************************************************//
+// Universidad del Valle de Guatemala                           //
+// Algoritmos y Estructuras de Datos                            //
+// Sección 30                                                   //
+// Mario Alejandro Alvarez Vidal	          Carné: 12429      //
+// Daniela Villamar                           Carné: 19086      // 
+// Mirka Monzón                               Carné: 18139      //
+// Título: Laboratorio 01 - Repeticion                          //
+// Fecha: 25 de enero de 2020                                   //
+// Nombre de Archivo: driverRadio.java                          //
+// Breve Descripción: Programa que simula un radio que sintoniza//
+// frecuencias AM/FM y guarda/borra estaciones en 12 sets       //
+//**************************************************************//
+
+public class driverRadio{
+
+	public static void main(String[] args) {
+		//Program Execution by RadioGUI method creation
+		RadioGUI radio = new RadioGUI();
+		radio.run();
+		
+	}
+
+}
